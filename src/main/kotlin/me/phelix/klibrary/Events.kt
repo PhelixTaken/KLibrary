22b04b3b29f6a4235de94fa5ec9f85d2b9f64dfa
PhelixTaken <thegggameyt@gmail.com>
2020-08-20 11:25:43 +0200

    Added event listening

---
+++ src/main/kotlin/me/phelix/klibrary/Events.kt
@@ -0,0 +1,15 @@
+package me.phelix.klibrary
+
+import org.bukkit.event.Event
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.plugin.java.JavaPlugin
+
+inline fun <reified T : Event> JavaPlugin.listen(
+    priority: EventPriority = EventPriority.NORMAL,
+    ignoreCancelled: Boolean = false,
+    crossinline callback: (T) -> Unit) = server.pluginManager.registerEvent(
+    T::class.java, object : Listener {},
+    priority, { _, it -> if(it is T) callback(it)},
+    this, ignoreCancelled
+)
