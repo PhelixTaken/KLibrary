79bd5a439784ff270952772e6a4b8f99bf81c38a
PhelixTaken <thegggameyt@gmail.com>
2020-08-20 15:38:43 +0200

    Added plugin messaging and disabling if no depends where found

--- src/main/kotlin/me/phelix/klibrary/KLibrary.kt
+++ src/main/kotlin/me/phelix/klibrary/KLibrary.kt
@@ -8,6 +8,25 @@ class KLibrary : JavaPlugin() {
 
     override fun onEnable() {
         Bukkit.getPluginManager().registerEvents(MenuListener(), this)
+        var contains = false
+        val list = mutableListOf<String>()
+        Bukkit.getPluginManager().plugins.forEach {
+            if(it.description.depend.contains(name) || it.description.softDepend.contains(name)) {
+                contains = true
+                list.add(it.name)
+            }
+        }
+
+        if(contains) {
+            println("KLibrary has found plugin(s) that use this as dependency")
+            list.forEach {
+                println("- $it")
+            }
+        } else {
+            println("KLibrary has not found any plugin using this as dependency so we are disabling this library to save performance!")
+            Bukkit.getPluginManager().disablePlugin(this)
+        }
+
     }
 
 }
